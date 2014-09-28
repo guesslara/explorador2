e28834693ac3005ddb4c37b67636fb097ded79d0
Elizabeth <Elizabeth>
2014-09-28 00:21:17 -0500

    Prueba Merge

--- config.php
+++ config.php
@@ -13,4 +13,5 @@
 
     $config['explorador']['pruebaEly2']="Aveces si te creo";//ruta que se va a explorar dentro del directorio
     $config['explorador']['pruebaEly2']="Trabajo colaborativo";//ruta que se va a explorar dentro del directorio
+    $config['explorador']['pruebaEly2']="Trabajo colaborativo gerardo";//ruta que se va a explorar dentro del directorio
 ?>
